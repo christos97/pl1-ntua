afd4494d8ef992908c411468132e1a8b1bbe2be4
Christos Sirmakezis <sirmchristos@gmail.com>
2021-03-21 14:56:36 +0200

    v1.1.0 - minor refactoring and macros added

--- set1/cpp/loop_rooms/loop_rooms.cpp
+++ set1/cpp/loop_rooms/loop_rooms.cpp
@@ -4,23 +4,25 @@
 #include <fstream>
 #include <string>
 #include <list>
-#include <stack>
 
 using namespace std;
+
 #define debug(x) cout << #x << " = " << x << endl
+#define ins(set, vertex) set.insert(vertex)
+typedef set<int> si; 
+typedef list<int> li;
 
 ifstream file;
 string out;
-set<int> winnable; // Once you reach you can win (already registered as winnable room)
-set<int> blacklist; // Once you reach you can't win (already registered as not winnable room)
+int n, m; /* Grid Dimensions */
 enum Direction { UP = 28, DOWN = 11, RIGHT = 25, LEFT = 19, OUT_OF_BOUNDS = 9999 };
-int n, m;
-bool existsInWinnable(int& next);
-//bool isInBlacklist(int& current);
+si winnable; /* Once you reach you can win (already registered as winnable room) */
+bool existsInSet(si& _set, int& _adj_node);
+
 class Graph {
     private:
         int nodes;
-        list<int> *adjlist;
+        li *adjlist;
         vector<bool> visited;
 
     public:
@@ -28,7 +30,7 @@ class Graph {
         }
 
         Graph (int nodes) { 
-            adjlist = new list<int> [nodes];
+            adjlist = new li[nodes];
             visited.resize(nodes, false);
             this->nodes = nodes;
         }
@@ -41,47 +43,15 @@ class Graph {
             adjlist[src].push_back(dst);
         }
 
-
-        // DFS recursive
         void dfs (int src) {
             visited[src] = true;
             for (auto& adj_node : adjlist[src]) {
                 if (adj_node == OUT_OF_BOUNDS) continue;
-                if (existsInWinnable(adj_node)) { /* If neighboor is winnable, src is winnable too */
-                    winnable.insert(src);
-                    debug(src);
-                    debug(adj_node);
-                    cout << endl;
-                    continue;
-                }
-                if (!visited[adj_node]) {
+                if (existsInSet(winnable, adj_node)) { ins(winnable, src); continue; }
+                if (!visited[adj_node]) 
                     dfs(adj_node);
-                }
             }
         } 
-
-    // DFS iterative
-        void dfsIterative (int src) {
-            stack<int> stk;
-            visited[src] = true;
-            stk.push(src);
-            while (!stk.empty()) {
-                src = stk.top();
-                stk.pop();
-                for (auto &adj_node : adjlist[src]) {
-                    if (adj_node == OUT_OF_BOUNDS) continue; // perimeter
-                    if (existsInWinnable(adj_node)) {
-                        winnable.insert(src);
-                        debug(adj_node);
-                        continue;
-                    }
-                    if (!visited[adj_node]) {
-                        visited[adj_node] = true;
-                        stk.push(adj_node);
-                    }
-                }
-            }
-        }
 
         void next () {
             fill(visited.begin(), visited.end(), false);
@@ -94,7 +64,7 @@ int main(int argc, char **argv) {
     file >> out; n = stoi(out);
     file >> out; m = stoi(out);
     Graph graph(n*m);
-    int vertex = 0, row = -1;
+    int v = 0, row = -1;
     while (file >> out) {
         row++;
         for (int col = 0; col < m ; col++){
@@ -104,67 +74,60 @@ int main(int argc, char **argv) {
                 switch (direction){
                     case UP: {
                         if (row == 0) {
-                            graph.addEdge(vertex, OUT_OF_BOUNDS);
-                            winnable.insert(vertex);
+                            graph.addEdge(v, OUT_OF_BOUNDS);
+                            ins(winnable, v);
                         }
-                        else graph.addEdge(vertex, vertex - m);
+                        else graph.addEdge(v, v - m);
                         break;
                     }
                     case DOWN: {
                         if (row == n-1) {
-                            graph.addEdge(vertex, OUT_OF_BOUNDS);
-                            winnable.insert(vertex);
+                            graph.addEdge(v, OUT_OF_BOUNDS);
+                            ins(winnable, v);
                         }
-                        else graph.addEdge(vertex, vertex + m);
+                        else graph.addEdge(v, v + m);
                         break;
                     }
                     case LEFT: {
                         if (col == 0) {
-                            graph.addEdge(vertex, OUT_OF_BOUNDS);
-                            winnable.insert(vertex);
+                            graph.addEdge(v, OUT_OF_BOUNDS);
+                            ins(winnable, v);
                         }
-                        else graph.addEdge(vertex, vertex - 1);
+                        else graph.addEdge(v, v - 1);
                         break;
                     }
                     case RIGHT: {
                         if (col == m-1) {
-                            graph.addEdge(vertex, OUT_OF_BOUNDS);
-                            winnable.insert(vertex);
+                            graph.addEdge(v, OUT_OF_BOUNDS);
+                            ins(winnable, v);
                         }
-                        else graph.addEdge(vertex, vertex + 1);
+                        else graph.addEdge(v, v + 1);
                         break;
                     }
                     default: perror("invalid char"); exit(1);
                 }
-                vertex++;
+                v++;
             }
             else {
                 switch (direction){
-                    case UP: graph.addEdge(vertex, vertex - m); break;
-                    case DOWN: graph.addEdge(vertex, vertex + m); break;
-                    case LEFT: graph.addEdge(vertex, vertex - 1); break;
-                    case RIGHT: graph.addEdge(vertex, vertex + 1); break;
+                    case UP: graph.addEdge(v, v - m); break;
+                    case DOWN: graph.addEdge(v, v + m); break;
+                    case LEFT: graph.addEdge(v, v - 1); break;
+                    case RIGHT: graph.addEdge(v, v + 1); break;
                     default: perror("invalid char"); exit(1);
                 }
-                vertex++;
+                v++;
             }
         }
     } 
     file.close();
-    
     for (int i=0; i < n*m; i++){
-        graph.dfs(i);
-        graph.next();
+        graph.dfs(i); graph.next();
     }
-
     int doomed = n*m - winnable.size();
-    debug(doomed);
+    cout<< doomed << endl;
 }
 
-/* bool isInBlacklist(int& current) {
-    return blacklist.find(current) != blacklist.end();
-} */
-
-inline bool existsInWinnable(int& next){
-    return winnable.find(next) != winnable.end();
+inline bool existsInSet(si& _set, int& _adj_node){
+    return _set.find(_adj_node) != _set.end();
 }
