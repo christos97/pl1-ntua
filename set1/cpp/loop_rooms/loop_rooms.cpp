f0e43b11b4a12cc65a6676b11529cc27f60b4aa3
Christos Sirmakezis <sirmchristos@gmail.com>
2021-03-21 15:39:58 +0200

    v1.2.0 - minified file reading

--- set1/cpp/loop_rooms/loop_rooms.cpp
+++ set1/cpp/loop_rooms/loop_rooms.cpp
@@ -4,20 +4,18 @@
 #include <fstream>
 #include <string>
 #include <list>
-
 using namespace std;
-
 #define debug(x) cout << #x << " = " << x << endl
-#define ins(set, vertex) set.insert(vertex)
+#define ins(_set, _v) _set.insert(_v)
 typedef set<int> si; 
 typedef list<int> li;
 
 ifstream file;
 string out;
 int n, m; /* Grid Dimensions */
-enum Direction { UP = 28, DOWN = 11, RIGHT = 25, LEFT = 19, OUT_OF_BOUNDS = 9999 };
+enum Direction { UP = 28, DOWN = 11, RIGHT = 25, LEFT = 19, OUT_OF_BOUNDS = 9999 }; /* So we don't hurt our eyes */
 si winnable; /* Once you reach you can win (already registered as winnable room) */
-bool existsInSet(si& _set, int& _adj_node);
+bool existsInSet(si& _set, int& _v);
 
 class Graph {
     private:
@@ -26,36 +24,16 @@ class Graph {
         vector<bool> visited;
 
     public:
-        Graph() {
+        Graph() { }
+        Graph (int _nodes) { 
+            adjlist = new li[_nodes];
+            visited.resize(_nodes, false);
+            this->nodes = _nodes;
         }
-
-        Graph (int nodes) { 
-            adjlist = new li[nodes];
-            visited.resize(nodes, false);
-            this->nodes = nodes;
-        }
-
-        ~Graph () { 
-            delete [] adjlist;
-        }
-
-        void addEdge (int src, int dst) {
-            adjlist[src].push_back(dst);
-        }
-
-        void dfs (int src) {
-            visited[src] = true;
-            for (auto& adj_node : adjlist[src]) {
-                if (adj_node == OUT_OF_BOUNDS) continue;
-                if (existsInSet(winnable, adj_node)) { ins(winnable, src); continue; }
-                if (!visited[adj_node]) 
-                    dfs(adj_node);
-            }
-        } 
-
-        void next () {
-            fill(visited.begin(), visited.end(), false);
-        }
+        ~Graph () { delete [] adjlist; }
+        void addEdge(int _src, int _dst);
+        void dfs(int _src);
+        void next();
 };
 
 int main(int argc, char **argv) {
@@ -64,63 +42,41 @@ int main(int argc, char **argv) {
     file >> out; n = stoi(out);
     file >> out; m = stoi(out);
     Graph graph(n*m);
-    int v = 0, row = -1;
+    int v = 0, row = -1; /* Every cell grid (vertex) has its own integer numbering */
     while (file >> out) {
         row++;
         for (int col = 0; col < m ; col++){
             int direction = out[col] - '9';
             bool perimeter = row == 0 || row == n-1 || col == 0 || col == m-1;
-            if (perimeter) { /* Room with OUT_OF_BOUNDS edge is winnable */
-                switch (direction){
-                    case UP: {
-                        if (row == 0) {
-                            graph.addEdge(v, OUT_OF_BOUNDS);
-                            ins(winnable, v);
-                        }
-                        else graph.addEdge(v, v - m);
-                        break;
-                    }
-                    case DOWN: {
-                        if (row == n-1) {
-                            graph.addEdge(v, OUT_OF_BOUNDS);
-                            ins(winnable, v);
-                        }
-                        else graph.addEdge(v, v + m);
-                        break;
-                    }
-                    case LEFT: {
-                        if (col == 0) {
-                            graph.addEdge(v, OUT_OF_BOUNDS);
-                            ins(winnable, v);
-                        }
-                        else graph.addEdge(v, v - 1);
-                        break;
-                    }
-                    case RIGHT: {
-                        if (col == m-1) {
-                            graph.addEdge(v, OUT_OF_BOUNDS);
-                            ins(winnable, v);
-                        }
-                        else graph.addEdge(v, v + 1);
-                        break;
-                    }
-                    default: perror("invalid char"); exit(1);
+            switch (direction){
+                case UP: {
+                    if (row == 0 && perimeter) { graph.addEdge(v, OUT_OF_BOUNDS); ins(winnable, v); }
+                    else graph.addEdge(v, v - m);
+                    break;
                 }
-                v++;
-            }
-            else {
-                switch (direction){
-                    case UP: graph.addEdge(v, v - m); break;
-                    case DOWN: graph.addEdge(v, v + m); break;
-                    case LEFT: graph.addEdge(v, v - 1); break;
-                    case RIGHT: graph.addEdge(v, v + 1); break;
-                    default: perror("invalid char"); exit(1);
+                case DOWN: {
+                    if (row == n-1 && perimeter) { graph.addEdge(v, OUT_OF_BOUNDS); ins(winnable, v); }
+                    else graph.addEdge(v, v + m);
+                    break;
                 }
-                v++;
+                case LEFT: {
+                    if (col == 0 && perimeter) { graph.addEdge(v, OUT_OF_BOUNDS); ins(winnable, v); }
+                    else graph.addEdge(v, v - 1);
+                    break;
+                }
+                case RIGHT: {
+                    if (col == m-1 && perimeter) { graph.addEdge(v, OUT_OF_BOUNDS); ins(winnable, v); }
+                    else graph.addEdge(v, v + 1);
+                    break;
+                }
+                default: perror("invalid char"); exit(1);
             }
+            v++;
         }
     } 
     file.close();
+
+    /* Solve */
     for (int i=0; i < n*m; i++){
         graph.dfs(i); graph.next();
     }
@@ -128,6 +84,24 @@ int main(int argc, char **argv) {
     cout<< doomed << endl;
 }
 
-inline bool existsInSet(si& _set, int& _adj_node){
-    return _set.find(_adj_node) != _set.end();
+
+void Graph::addEdge (int _src, int _dst) {
+            adjlist[_src].push_back(_dst);
+        }
+
+void Graph::dfs (int _src) {
+            visited[_src] = true;
+            for (auto& adj_node : adjlist[_src]) {
+                if (adj_node == OUT_OF_BOUNDS) continue; /* Perimeter is checked at file reading */
+                if (existsInSet(winnable, adj_node)) { ins(winnable, _src); continue; } /* If my neighboor is winnable, im winnable */
+                if (!visited[adj_node]) 
+                    dfs(adj_node);
+            }
+        } 
+void Graph::next () {
+            fill(visited.begin(), visited.end(), false);
+        }
+
+inline bool existsInSet(si& _set, int& _v){
+    return _set.find(_v) != _set.end();
 }
