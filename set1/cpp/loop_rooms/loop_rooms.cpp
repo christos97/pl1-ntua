e39edb74a3715affbbdbb0f6e9ea65cac1e08743
Christos Sirmakezis <sirmchristos@gmail.com>
2021-03-22 12:06:20 +0200

    v1.4.0 - added parent to dfs + doomed set same logic as winnable set, to catch loops and already blacklisted nodes

--- set1/cpp/loop_rooms/loop_rooms.cpp
+++ set1/cpp/loop_rooms/loop_rooms.cpp
@@ -7,12 +7,12 @@
 #include <chrono>
 using namespace std;
 #define debug(x) cout << #x << " = " << x << endl
+#define append(src, dst) dst.insert(src.begin(), src.end())
 typedef set<int> si; 
 typedef list<int> li;
 
 enum Direction { UP = 28, DOWN = 11, RIGHT = 25, LEFT = 19, OUT_OF_BOUNDS = 9999 }; 
 int n, m; /* Grid size */
-si winnable; /* Once you reach you can win (already registered as winnable room) */
 bool existsInSet(si& _set, int& _v);
 
 class Graph {
@@ -21,10 +21,12 @@ class Graph {
         li *adjlist;
         vector<bool> visited;
         void addEdge(int _src, int _dst);
-        void dfs(int _src);
+        void dfs(int& _src, int& _par);
         void next();
 
     public:
+        si winnable; /* Once you reach you can win */
+        si doomed; /* Once you reach you can't win */
         Graph() { }
         Graph (int _nodes) { 
             adjlist = new li[_nodes];
@@ -34,12 +36,14 @@ class Graph {
         ~Graph () { delete [] adjlist; }
         void from(ifstream& _file);
         int countDoomed();
+        void printRooms();
+
 };
 
 int main(int argc, char **argv) {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     auto start = chrono::steady_clock::now();
-
+    
     ifstream file;
     string gridSize;
     file.open(argv[1]);
@@ -48,12 +52,13 @@ int main(int argc, char **argv) {
     Graph graph(n*m);
     graph.from(file);
     file.close();
-    int doomed = graph.countDoomed();
-    debug(doomed);
+    int res = graph.countDoomed();
+    cout << res << endl;
+
+    graph.printRooms();
     auto end = chrono::steady_clock::now();
     auto diff = end - start;
     cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
-    for (auto winab: winnable) debug(winab);
 }
 
 inline bool existsInSet(si& _set, int& _v) {
@@ -61,37 +66,34 @@ inline bool existsInSet(si& _set, int& _v) {
 }
 
 /* Private */
-void Graph::addEdge (int _src, int _dst) {
+void Graph::addEdge(int _src, int _dst) {
     adjlist[_src].push_back(_dst);
 }
 
-void Graph::dfs (int _src) {
-    visited[_src] = true;
-    for (auto& adj_node : adjlist[_src]) {
-        if (adj_node == OUT_OF_BOUNDS) continue; /* Perimeter is checked at file reading */
-        if (existsInSet(winnable, adj_node)) { /* My neighboor can win, so i can win too */
-            for (size_t i = 0; i <= visited.size(); i++) {
-                if (visited[i]) winnable.insert(i); 
-            } 
-            continue; 
-        }
-        if (!visited[adj_node]) 
-            dfs(adj_node);
+si visited_set; /* temp memory to append faster this round's visited nodes, rather than looping through n*m visited vector to find true values */
+void Graph::dfs(int& _src, int& _par) {
+    visited[_src] = true; visited_set.insert(_src);
+    for (int& adj_node : adjlist[_src]) {
+        if (adj_node == OUT_OF_BOUNDS) continue; /* Perimeter checked at file reading, skip */
+        if (adj_node == _par) { append(visited_set, doomed); continue; } /* I looped, we are all doomed */
+        if (existsInSet(winnable, adj_node)) { append(visited_set, winnable); break; } /* I reached a winnable room, we can all win */
+        if (existsInSet(doomed, adj_node)) { append(visited_set, doomed); break; } /* I reached a doomed room, we are all doomed */
+        if (!visited[adj_node]) dfs(adj_node, _src); /* we still need the vector for 0(1) here, existsInSet would be overkill */
     }
 } 
 
-void Graph::next () {
+void Graph::next() {
     fill(visited.begin(), visited.end(), false);
+    visited_set.clear();
 }
 
 /* Public */
 void Graph::from(ifstream& _file) {
-    int row = -1, v = 0;
+    int row = 0, v = 0;
     string out;
-    while (_file >> out) {
-        row++;
-        for (int col = 0; col < m ; col++){
-            switch (out[col] - '9'){
+    while (_file >> out) { /* File has no spaces so every loop reads one row */
+        for (int col = 0; col < m ; col++){ /* Pick a char */
+            switch (out[col] - '9'){ /* Switch direction and save winnable rooms in grid perimeter */
                 case UP: {
                     if (row == 0) { addEdge(v, OUT_OF_BOUNDS); winnable.insert(v); }
                     else addEdge(v, v - m);
@@ -116,13 +118,21 @@ void Graph::from(ifstream& _file) {
             }
             v++;
         }
+        row++;
     }
 }
 
 int Graph::countDoomed() {
-    for (int i=0; i < n*m; i++){ dfs(i); next(); }
-    int winab_count = winnable.size();
-    debug(n*m);
-    debug(winab_count);
+    int par = -1;
+    for (int i=0; i < n*m; i++){ 
+        dfs(i, par); 
+        debug(par);
+        next(); 
+    }
     return n*m - winnable.size();
+}
+
+void Graph::printRooms() {
+    for (auto winnab: winnable) debug(winnab);
+    for (auto doomd: doomed) debug(doomd);
 }
