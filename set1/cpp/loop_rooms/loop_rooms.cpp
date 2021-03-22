ad4b3eeb01acc98c35c5c53695807f43bdc10204
Christos Sirmakezis <sirmchristos@gmail.com>
2021-03-22 12:43:52 +0200

    v1.4.5 - added some docs

--- set1/cpp/loop_rooms/loop_rooms.cpp
+++ set1/cpp/loop_rooms/loop_rooms.cpp
@@ -11,14 +11,15 @@ using namespace std;
 typedef set<int> si; 
 typedef list<int> li;
 
-enum Direction { UP = 28, DOWN = 11, RIGHT = 25, LEFT = 19, OUT_OF_BOUNDS = 9999 }; 
-int n, m; /* Grid size */
+enum Direction { UP = 28, DOWN = 11, RIGHT = 25, LEFT = 19, OUT_OF_BOUNDS = 9999 }; /* Acceptable moves */
+int n; /* Grid rows */ 
+int m; /* Grid cols */
 bool existsInSet(si& _set, int& _v);
 
 class Graph {
     private:
-        int nodes;
-        li *adjlist;
+        int nodes; /* Number pf nodes */
+        li *adjlist; /* Graphs adjacency list */
         vector<bool> visited;
         void addEdge(int _src, int _dst);
         void dfs(int& _src, int& _par);
@@ -29,7 +30,7 @@ class Graph {
         si doomed; /* Once you reach you can't win */
         Graph() { }
         Graph (int _nodes) { 
-            adjlist = new li[_nodes];
+            adjlist = new li[_nodes]; 
             visited.resize(_nodes, false);
             this->nodes = _nodes;
         }
@@ -61,16 +62,37 @@ int main(int argc, char **argv) {
     cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
 }
 
+/** 
+ *  Search if a value exists in a set
+ *  @param _set: Set to search integer value
+ *  @param _v: Value to search for
+ *  @returns true if found, false otherwise
+*/
 inline bool existsInSet(si& _set, int& _v) {
     return _set.find(_v) != _set.end();
 }
 
-/* Private */
+/* Private Methods */
+
+/** 
+ *  Create a directed edge between two nodes 
+ *  @param _src: from node  
+ *  @param _dst: to node
+ *  @returns void
+*/
 void Graph::addEdge(int _src, int _dst) {
     adjlist[_src].push_back(_dst);
 }
 
-si visited_set; /* temp memory to append faster this round's visited nodes, rather than looping through n*m visited vector to find true values */
+/* Temp memory to append faster this round's visited nodes, rather than looping through n*m visited vector */
+si visited_set; 
+
+/** 
+ * Recursive dfs that also searches for values in winnable and/or doomed sets before continuing.
+ *  @param _src: source node to dfs 
+ *  @param _par: parent of current source node
+ *  @return void    
+*/
 void Graph::dfs(int& _src, int& _par) {
     visited[_src] = true; visited_set.insert(_src);
     for (int& adj_node : adjlist[_src]) {
@@ -82,12 +104,18 @@ void Graph::dfs(int& _src, int& _par) {
     }
 } 
 
+/* Clear visited memory for next round */
 void Graph::next() {
     fill(visited.begin(), visited.end(), false);
     visited_set.clear();
 }
 
-/* Public */
+/* Public Methods */
+
+/** 
+    * @param _file: File to create graph from
+    * @returns void or exits program if file invalid 
+*/
 void Graph::from(ifstream& _file) {
     int row = 0, v = 0;
     string out;
@@ -122,6 +150,10 @@ void Graph::from(ifstream& _file) {
     }
 }
 
+/**
+ *  Call after graph is created from file
+ *  @returns Number of doomed rooms in grid  
+*/
 int Graph::countDoomed() {
     int par = -1;
     for (int i=0; i < n*m; i++){ 
@@ -132,6 +164,7 @@ int Graph::countDoomed() {
     return n*m - winnable.size();
 }
 
+/* Print helper */
 void Graph::printRooms() {
     for (auto winnab: winnable) debug(winnab);
     for (auto doomd: doomed) debug(doomd);
